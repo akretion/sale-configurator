e4a45343131a5618c10c6cd05366f48328693109
Mourad <mourad.elhadj.mimoune@akretion.com>
2020-04-17 11:27:27 +0200

    [ADD] sale line conf price total test

--- sale_configurator_option/tests/test_sale_order.py
+++ sale_configurator_option/tests/test_sale_order.py
@@ -11,8 +11,34 @@ class SaleOrderCase(SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.sale = cls.env.ref("sale_configurator_option.sale_order_1")
+        cls.line_with_opt = cls.env.ref(
+                "sale_configurator_option.sale_order_line_1")
+        cls.line_opt_1 = cls.env.ref(
+                "sale_configurator_option.sale_order_line_option_1")
+        cls.line_opt_2 = cls.env.ref(
+                "sale_configurator_option.sale_order_line_option_2")
+        cls.line_opt_3 = cls.env.ref(
+                "sale_configurator_option.sale_order_line_option_3")
 
     def test_total_amount(self):
         self.assertEqual(self.sale.amount_total, 126.50)
         self.assertEqual(self.sale.amount_untaxed, 110)
         self.assertEqual(self.sale.amount_tax, 16.5)
+
+    def test_conf_total_amount(self):
+        self.assertEqual(self.line_with_opt.price_subtotal, 0)
+        self.assertEqual(self.line_with_opt.price_config_subtotal, 110)
+        self.assertEqual(self.line_with_opt.price_config_total, 126.5)
+        # Set price to product with option and check totals
+        cls.line_with_opt.price_unit = 100
+        self.assertEqual(self.line_with_opt.price_subtotal, 100)
+        self.assertEqual(self.line_with_opt.price_config_subtotal, 210)
+        self.assertEqual(self.line_with_opt.price_total, 115)
+        self.assertEqual(self.line_with_opt.price_config_total, 241.5)
+        self.assertEqual(self.line_opt_1.price_config_total, 0)
+        self.assertEqual(self.line_opt_2.price_config_total, 0)
+        self.assertEqual(self.line_opt_3.price_config_total, 0)
+
+        self.assertEqual(self.sale.amount_total, 241.5)
+        self.assertEqual(self.sale.amount_untaxed, 210)
+        self.assertEqual(self.sale.amount_tax, 31.5)
