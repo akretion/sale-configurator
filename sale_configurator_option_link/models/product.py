f6459a54c23fc01c9048f5c29a8ecfcd0b18556e
Mourad <mourad.elhadj.mimoune@akretion.com>
2020-05-22 10:39:11 +0200

    [FIX] included_by_product constraint & compute

--- sale_configurator_option_link/models/product.py
+++ sale_configurator_option_link/models/product.py
@@ -24,17 +24,18 @@ class ProductConfiguratorOption(models.Model):
         "product.configurator.option", "included_by_option_id"
     )
 
-    @api.one
+    @api.multi
     @api.constrains("included_by_product_id", "included_by_option_id")
     def _check_included_by_product_id(self):
-        if self.included_by_product_id and not self.included_by_option_id:
+        if self.filtered(lambda o: o.included_by_product_id and
+                not o.included_by_option_id):
             raise ValidationError(_("Included option must be defined first"))
 
     @api.multi
     @api.depends("included_by_product_id")
     def _compute_included_by_option_id(self):
         for option in self:
-            included_by_option_ids = self.product_tmpl_id.\
+            included_by_option_ids = option.product_tmpl_id.\
                 configurable_option_ids.filtered(
                     lambda o: o.product_id == option.included_by_product_id
                 )
