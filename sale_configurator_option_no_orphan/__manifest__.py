92038094edf97cdace07170068f615a23b285583
Kevin Khao <kevinkhao@gmail.com>
2021-08-27 12:46:53 +0300

    [ADD] sale_configurator_option_no_orphan: Add

---
+++ sale_configurator_option_no_orphan/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Akretion (http://www.akretion.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Sale Configurator Option No Orphan",
+    "summary": "Remove ability to add options as standard sale order lines",
+    "version": "14.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://github.com/akretion/sale-configurator",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["sale_configurator_option"],
+    "data": ["views/sale_order_view.xml"],
+}
