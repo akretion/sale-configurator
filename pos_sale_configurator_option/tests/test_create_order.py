8e8e1679d5366257a2d44294f62930605b8c247f
Sébastien BEAU <sebastien.beau@akretion.com>
2025-10-21 17:05:02 +0200

    pos_sale_configurator_option: set module in post install

--- pos_sale_configurator_option/tests/test_create_order.py
+++ pos_sale_configurator_option/tests/test_create_order.py
@@ -2,9 +2,12 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+from odoo.tests import tagged
+
 from .common import CommonOptionCase
 
 
+@tagged("post_install", "-at_install")
 class TestCreateOrder(CommonOptionCase):
     def test_create_sale(self):
         data = self._get_pos_data()
